1d8ac8b19822f7a80eaee3cfcd92f56fd0d03b1e
User <muthomidenis62@gmail.com>
2022-06-19 12:23:44 +0300

    Fetch database data

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,20 @@ public class SignUp extends AppCompatActivity {
 
     private TextInputEditText fullname, email, contact, username, password;
     private Button buttonSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        fullname = findViewById(R.id.fullname);
+        email = findViewById(R.id.email);
+        contact = findViewById(R.id.contact);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+
+    }
 
 
 }
