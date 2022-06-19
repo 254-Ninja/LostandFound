77ed3b7cd9822e9fdc5b2354697eebe5ff946d3e
User <muthomidenis62@gmail.com>
2022-06-19 12:17:35 +0300

    Fetch database data

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,10 +1,16 @@
 package com.example.myapplication;
 
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class SignUp extends AppCompatActivity {
+
+    private TextInputEditText fullname, email, contact, username, password;
+    private Button buttonSignUp;
 
 
 }
