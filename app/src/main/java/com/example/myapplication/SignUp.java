b040fea46da521d7a9645f6804b5f4f14b72bb42
User <muthomidenis62@gmail.com>
2022-06-19 13:07:59 +0300

    Fetch database data

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class SignUp extends AppCompatActivity {
@@ -43,6 +55,61 @@ public class SignUp extends AppCompatActivity {
 
     private void createUser(final String fullname,final String email, final String username, final String contact, final String password){
 
+        mRequestQueue = Volley.newRequestQueue(SignUp.this);
+        // Progress
+        buttonSignUp.setText("Creating User...");
+
+        mStringRequest = new StringRequest(Request.Method.POST, getBaseUrl(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    String success = jsonObject.getString("success");
+                    String message = jsonObject.getString("message");
+
+                    if (success.equals("1")) {
+
+                        Toast.makeText(SignUp.this,message,Toast.LENGTH_SHORT).show();
+                        buttonSignUp.setText("Sign Up");
+
+
+                    }
+
+                } catch (JSONException e) {
+
+                    Toast.makeText(SignUp.this,e.toString(),Toast.LENGTH_LONG).show();
+                    buttonSignUp.setText("Sign Up");
+
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(SignUp.this,error.toString(),Toast.LENGTH_LONG).show();
+                buttonSignUp.setText("Sign Up");
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params = new HashMap<>();
+                params.put("fullname",fullname);
+                params.put("email",email);
+                params.put("username",username);
+                params.put("contact",contact);
+                params.put("password",password);
+
+                return params;
+            }
+        };
+
+        mStringRequest.setShouldCache(false);
+        mRequestQueue.add(mStringRequest);
     }
 
     private String getBaseUrl (){
