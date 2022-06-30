c40e05616af9bf8042477ba81c6c33c6408da474
User <muthomidenis62@gmail.com>
2022-06-30 16:23:02 +0300

    Latitude Longitude

--- app/src/main/java/com/example/myapplication/Geolocation.java
+++ app/src/main/java/com/example/myapplication/Geolocation.java
@@ -1,4 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Context;
+
+import java.util.logging.Handler;
+
 public class Geolocation {
+
+    public static void getAddress(String locationAddress, Context context, Handler handler){
+
+    }
 }
