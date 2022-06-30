7892e56fc829966e23e249da8bba2a9a8a2f702d
User <muthomidenis62@gmail.com>
2022-06-30 16:43:08 +0300

    Latitude Longitude

--- app/src/main/java/com/example/myapplication/Geolocation.java
+++ app/src/main/java/com/example/myapplication/Geolocation.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.location.Address;
 import android.location.Geocoder;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 import java.util.logging.Handler;
@@ -16,7 +18,15 @@ public class Geolocation {
             public void run() {
                 Geocoder geocoder = new Geocoder(context, Locale.getDefault());
                 String result = null;
-                List addressList = geocoder.getFromLocationName(locationAddress,1);
+                try {
+                    List addressList = geocoder.getFromLocationName(locationAddress,1);
+                    if (addressList != null && addressList.size() > 0){
+                        Address address = (Address) addressList.get(0);
+                        StringBuilder stringBuilder = new StringBuilder();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
