92f6a2b88573901e24117bad2b312613cd9875a7
User <muthomidenis62@gmail.com>
2022-07-19 22:16:55 +0300

    Found form

--- app/src/main/java/com/example/myapplication/foundForm.java
+++ app/src/main/java/com/example/myapplication/foundForm.java
@@ -3,8 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class foundForm extends AppCompatActivity {
+
+    EditText etPlace;
+    Button btSubmit;
+    TextView tvAddress;
+
+    private TextInputEditText GeneralDescription;
+
+    // Volley variables
+    private StringRequest mStringRequest;
+    private RequestQueue mRequestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
